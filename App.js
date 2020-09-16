b1f0d9f767010f815aa5b40daf57989a3dc8196c
dautokute9x <50734983+dautokute9x@users.noreply.github.com>
2020-09-16 16:36:52 +0700

    Update App.js

--- App.js
+++ App.js
@@ -25,15 +25,16 @@ import { useState } from 'react';
 
 
 class App1 extends Component {
+
   componentDidMount() {
     setInterval(() => (
       this.setState(previousState => ({ isShowingText: !previousState.isShowingText }))
 
-    ), 1000);
+    ), 2000);
   }
-  state = {isShowingText:true}
+  state = { isShowingText: true }
   render() {
-    if(!this.state.isShowingText){
+    if (!this.state.isShowingText) {
       return null
     }
     return (
@@ -41,21 +42,69 @@ class App1 extends Component {
     )
   }
 }
+class App2 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { message: 'this is state' }
+  }
+  render() {
+    return (
+      <Text>
+        {this.state.message}
+      </Text>
+    )
+  }
+}
+class App3 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: "Duy" }
+  }
+  render() {
+    return (
+      <View>
+        <Text>
+          {this.props.name}
 
+        </Text>
+        <Text>
+          {this.state.text}
+        </Text>
+      </View>)
+  }
+}
+
+class App4 extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  updateState = (props) => this.setState({ myState: 'updated' })
+  render() {
+    return (
+      <View>
+        <Text onPress={this.updateState}> {this.props.myState} </Text>
+      </View>
+    );
+  }
+}
 
 export default class App extends Component {
   constructor(props) {
     super(props)
-    
+    this.state = { name: '12232' }
   }
   render() {
     return (
       <View style={{ margin: 150 }}>
-        {/* <Text>{this.state.message}</Text> */}
-        <App1 text='Hung'></App1>
+
+
+
+        <App4 myState={this.state.name} ></App4>
+
       </View>
     )
   }
-} 
+}
 
 
