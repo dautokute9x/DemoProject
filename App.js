6e1943fcb557a933c0bdc98daa5cd4c6ff6c3a78
dautokute9x <50734983+dautokute9x@users.noreply.github.com>
2020-09-18 17:47:15 +0700

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import {
   Modal,
   View,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  Alert
 } from 'react-native';
 import { useState } from 'react';
 
@@ -16,7 +17,9 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     console.log('constructor')
-    this.state = { count: 1 }
+    console.log('<------------------>')
+
+    this.state = { count: 0 }
 
   }
   increment = () => { this.setState({ count: this.state.count + 1 }) }
@@ -24,41 +27,65 @@ export default class App extends Component {
 
   render() {
     console.log('render')
+    console.log('<------------------>')
+
     return (
       <SafeAreaView>
-      <Text style={{ textAlign: 'center' }}>{this.state.count}
-     </Text>
-     <ChildComponent childIncrement= {this.increment} childDecrement={this.decrement}> </ChildComponent>
-     </SafeAreaView>
+        <Text style={{ textAlign: 'center', marginTop: 250, fontSize: 50 }}>{this.state.count}
+        </Text>
+        <ChildComponent childIncrement={this.increment} childDecrement={this.decrement}> </ChildComponent>
+      </SafeAreaView>
     )
   }
-componentDidMount(){
-  console.log('component did mount' ,this.state.count)
+
+
+  componentDidMount() {
+    console.log('component did mount', this.state.count)
+    console.log('<------------------>')
+  }
+  componentDidUpdate() {
+    console.log('component did update', this.state.count)
+    console.log('<------------------>')
+
+  }
+  UNSAFE_componentWillMount() {
+    this.setState({ count: 99 })
+    console.log('component will mount')
+    console.log('<------------------>')
+
+  }
+  shouldComponentUpdate() {
+
+    console.log('should component update')
+    console.log('<------------------>')
+
+    return true
+  }
+  UNSAFE_componentWillUpdate() {
+
+    Alert.alert('Updated')
+
+    console.log('component wil update')
+    console.log('<------------------>')
+
+  }
 }
 
-componentDidUpdate(){
-  console.log('component did update', this.state.count)
-}
-static getDerivedStateFromProps(){
-  console.log('getDerived state from props')
-}
 
-} 
+class ChildComponent extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <SafeAreaView style={{ flexDirection: 'row', marginLeft: 180 }} >
 
-class ChildComponent extends Component{
-constructor(props){
-  super(props)
-}
-render(){
-  return(
-    <SafeAreaView>
-   
-    <Button onPress={this.props.childIncrement} title='+' >
-    </Button>
-    <Button onPress={this.props.childDecrement} title='-'></Button>
+        <Button onPress={this.props.childIncrement} title='+'  >
+        </Button>
+        <Button onPress={this.props.childDecrement} title='-'></Button>
 
-  </SafeAreaView>
-  )
-}
+      </SafeAreaView>
+    )
+  }
 
 }
